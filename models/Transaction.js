6165c8d8b5d9447adeda2ef20688d12d2b9955ae
alphathehacker <110968710+alphathehacker@users.noreply.github.com>
2025-06-20 20:45:37 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
-  customer: { type: String, required: true },
-  items: [
-    {
-      name: String,
-      price: Number
-    }
-  ],
-  amount: { type: Number, required: true },
-  type: { type: String, enum: ['paid', 'katha'], required: true }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  customer: String,
+  items: [{ name: String, price: Number }],
+  amount: Number,
+  type: String
 }, { timestamps: true });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
